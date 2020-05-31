03d2c2abb35261697316601b91c46547e87388ab
Alexander Zaytsev <hazzik@gmail.com>
2020-05-31 03:07:05 +0000

    Refactor to avoid CQS violation

--- ProjectionManager/WardViewProjection.cs
+++ ProjectionManager/WardViewProjection.cs
@@ -7,53 +7,49 @@ namespace ProjectionManager
     {
         public WardViewProjection(ConnectionFactory connectionFactory)
         {
-            When<PatientAdmitted>(e =>
+            When<PatientAdmitted>((e, p) =>
             {
-                using (var session = connectionFactory.Connect())
-                {
-                    session.Store(new Patient
-                    {
-                        Id = e.PatientId,
-                        WardNumber = e.WardNumber,
-                        PatientName = e.PatientName,
-                        AgeInYears = e.AgeInYears
-                    });
-
-                    session.SaveChanges();
-                }
-
                 Console.WriteLine($"Recording Patient Admission: {e.PatientName}");
+                return new Patient
+                {
+                    Id = e.PatientId,
+                    WardNumber = e.WardNumber,
+                    PatientName = e.PatientName,
+                    AgeInYears = e.AgeInYears
+                };
             });
 
-            When<PatientTransfered>(e =>
+            When<PatientTransfered>((e, p) =>
             {
-                using (var session = connectionFactory.Connect())
-                {
-                    var patient = session.Load<Patient>(e.PatientId);
-                    patient.WardNumber = e.WardNumber;
-                    session.SaveChanges();
-                }
-
                 Console.WriteLine($"Recording Patient Transfer: {e.PatientId}");
+                return new Patient(p)
+                {
+                    WardNumber = e.WardNumber
+                };
             });
 
-            When<PatientDischarged>(e =>
+            When<PatientDischarged>((e, p) =>
             {
-                using (var session = connectionFactory.Connect())
-                {
-                    var patient = session.Load<Patient>(e.PatientId);
-                    session.Delete(patient);
-
-                    session.SaveChanges();
-                }
-
                 Console.WriteLine($"Recording Patient Discharged: {e.PatientId}");
+                return null;
             });
         }
     }
 
     public class Patient
     {
+        public Patient()
+        {
+        }
+        
+        public Patient(Patient p)
+        {
+            Id = p.Id;
+            WardNumber = p.WardNumber;
+            PatientName = p.PatientName;
+            AgeInYears = p.AgeInYears;
+        }
+        
         public Guid Id { get; set; }
 
         public int WardNumber { get; set; }
